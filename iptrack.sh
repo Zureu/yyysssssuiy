3d5161c2db8c5e5360e88f82cc20fb050b287d16
Zureu <muharomifajar130@gmail.com>
2025-05-21 20:56:36 +0700

    Update iptrack.sh

--- iptrack.sh
+++ iptrack.sh
@@ -1,6 +1,6 @@
 pkg update && pkg upgrade -y
 pkg install wget -y
 pip uninstall python-telegram-bot -y
-wget -P $PREFIX/bin https://raw.githubusercontent.com/Zureu/toolsZureu/refs/heads/main/iptrack.py
+wget -P $PREFIX/bin https://raw.githubusercontent.com/Zureu/yyysssssuiy/refs/heads/main/iptrack.py
 chmod +x $PREFIX/bin/iptrack.py
 iptrack.py
